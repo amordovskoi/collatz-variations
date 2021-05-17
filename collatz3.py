5a82f4d65b0af55f7f043737a7786c3349a9dcc8
amordovskoi <41445626+amordovskoi@users.noreply.github.com>
2021-05-16 18:41:33 -0700

    Update collatz3.py

--- collatz3.py
+++ collatz3.py
@@ -8,18 +8,19 @@ def down_to_one(n, x):
 	print('n =', n)
 	n_orig = n
 	i=0
-	while n!=1:
+	while n != 1:
 		print(n)
-		i+=1
-		if (n%2)==0:
+		i += 1
+		if (n%2) == 0:
 			n=n//2
 		else:
-			n = (2**x)*n+2**x
+			n = (2**x) * n + 2**x
+#			n = 3*n + 1
 	return i
 
 
 n = 54717			## - starting number n
 x = 13111			## - any integer
 
-print('1 : Total Steps:', down_to_one(n, x))
+print('1 : Total Steps:', down_to_one(n,x))
 print('n =', n, ', x =', x)
